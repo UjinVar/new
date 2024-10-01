52cc198ef9e864fa9d13d11f066625af12e21227
UjinVar <power.voronko@yandex.ru>
2024-10-01 17:29:34 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Hello");
         //mediumArithmetic();
         //factorial();
         //costProduct();
